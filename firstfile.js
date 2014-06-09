4aa85284440db729ea18e8e95b10e82443e96c57
LeighMarie <leighmariebraswell@31-35-51.wireless.csail.mit.edu>
2014-06-09 11:36:01 -0400

    changes

--- firstfile.js
+++ firstfile.js
@@ -1,12 +1,31 @@
 var calculator = (function () {
-    function calculate(){
-        var txt = $('#text');
-        var val = txt.val();
-        $('#text_out').text(val);
+    function calculate(text){
+        //var txt = $('#text');
+        //var val = txt.val();
+        //$('#text_out').text(val);
         //console.log(val);
+        return text;
         }
     
+    function setup(div){
+        var input = $('<input></input>', {type: 'text', size: 50});
+        var output = $('<div></div>');
+        var button = $('<button>Calculate</button>');
+        $(div).append(input, button, output);
+        
+        button.on('click', function (event) {
+            output.text(calculate(input.val()));            
+    });       
+    }
+    
     return {
-        calculate: calculate
+        calculate: calculate,
+        setup: setup
     };
 })();
+
+$(document).ready(function () {
+    $('.calc').each(function () {
+        calculator.setup(this);
+    });
+});
